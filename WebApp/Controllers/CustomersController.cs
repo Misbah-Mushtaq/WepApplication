5f9c80b0015ef971b4ea8c4e3049c9946ac1cdad
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2019-12-15 01:15:17 -0800

    init

---
+++ WebApp/Controllers/CustomersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private AppEntities db = new AppEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var id = Convert.ToInt32(System.Web.HttpContext.Current.Session["LoginId"]);
+            var customers = db.Customers.Include(c => c.UserLogin).Where(x=>x.Id == id);
+            return View(customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.UserLogins, "Id", "Username");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerId,Id,Purchase,Date")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.UserLogins, "Id", "Username", customer.Id);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.UserLogins, "Id", "Username", customer.Id);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerId,Id,Purchase,Date")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.UserLogins, "Id", "Username", customer.Id);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using System.Data.Entity;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private AppEntities db = new AppEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(WebApp.Models.UserLogin userLogin)
+        {
+            var loginDetails = db.UserLogins.Where(x => x.Username == userLogin.Username && x.Password == userLogin.Password).FirstOrDefault();
+            if (loginDetails == null)
+            {
+                return RedirectToAction("Index", "Login");
+           
+            }
+            else
+            {
+                Session["LoginId"] = loginDetails.Id;
+                Session["LoginName"] = loginDetails.Name;
+                Session["LoginUserName"] = loginDetails.Username;
+               
+                return RedirectToAction("Index", "Customers"); 
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            //int Login = (int)Session["LoginId"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        
+        }
+    }
+}
